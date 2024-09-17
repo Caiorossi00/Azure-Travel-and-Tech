9a8e656e82af737505a1bc2f02f9b90acfdb7a63
Caiorossi00 <caio_rossi56@hotmail.com>
2024-09-17 12:40:02 -0300

    texto logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,13 @@ import "./Navbar.css";
 const Navbar = ({ toggleMenu }) => {
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
+      <a
+        href="https://www.instagram.com/caiorossi.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="logo">Caiorossi.dev</div>
+      </a>{" "}
       <div className="menu-icon" onClick={toggleMenu}>
         &#9776;
       </div>
